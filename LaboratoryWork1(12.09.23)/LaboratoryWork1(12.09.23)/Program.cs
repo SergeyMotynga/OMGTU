76c12f44175b30d142b3a0f234d3cc839cbb817c
Sergey Motynga <145015550+SergeyMotynga@users.noreply.github.com>
2023-09-16 17:25:13 +0600

    LaboratoryWork1(12.09.23)
    Лабораторная работа 1

---
+++ LaboratoryWork1(12.09.23)/LaboratoryWork1(12.09.23)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratoryWork1_12._09._23_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int S = 0, n = 5, m = 10, l = 7;
+            for (int i = 0; i <= 20; i++) 
+            {
+                S = i * (2 * (m + n + l) + m * (i - 1));
+                Console.WriteLine(S);
+            }
+        }    
+    }
+}
