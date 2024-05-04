8cb06a0562fb7082227608d26d08b1f6239ec0c3
Sergey Motynga <motynga12345@mail.ru>
2024-05-04 19:22:12 +0600

    1

---
+++ OL/1Term/KrestyaninIChert.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace krestyaninIChert
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение maxN: ");
+            int maxN = Convert.ToInt32(Console.ReadLine());
+            long result = maxN;
+            for (int i = 3; i <= maxN; i = 2 * i + 1)
+            {
+                int rem = maxN / i;
+                result += rem;
+            }
+            Console.WriteLine("Всего исходов: " + result);          
+        }
+    }
+}
---
+++ OL/1Term/Milk.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milk
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int l = 0;
+            float min = 1000f;
+            for (int i = 0; i < N; i++)
+            {
+                int x1 = Convert.ToInt32(Console.ReadLine());   
+                int y1 = Convert.ToInt32(Console.ReadLine());
+                int z1 = Convert.ToInt32(Console.ReadLine());
+                int x2 = Convert.ToInt32(Console.ReadLine());
+                int y2 = Convert.ToInt32(Console.ReadLine());
+                int z2 = Convert.ToInt32(Console.ReadLine());
+                float c1 = Convert.ToSingle(Console.ReadLine()); 
+                float c2 = Convert.ToSingle(Console.ReadLine());
+
+                int s1 = 2 * x1 * y1 + 2 * x1 * z1 + 2 * y1 * z1;   
+                int s2 = 2 * x2 * y2 + 2 * x2 * z2 + 2 * y2 * z2;
+
+                float v1 = (x1 * y1 * z1) / 1000f;        
+                float v2 = ((float)x2 * y2 * z2) / 1000;
+
+                float price = (c2 * s1 - s2 * c1) / (v2 * s1 - s2 * v1); 
+
+                if (price < min)
+                {
+                    l = i + 1;
+                    min = price;
+                }
+            }
+            Console.WriteLine("{0} {1:F}", l, min);
+        }
+
+    }
+}
