e2246753fb9cc8640c62013db55f1c8e351e9f7e
Sergey Motynga <motynga12345@mail.ru>
2024-05-04 19:23:06 +0600

    1

---
+++ LA/1Term/22.11pz.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace students
+{
+    class Student
+    {
+        string fio, group, year;
+        public Student(string fio, string group, string year)
+        {
+            this.fio = fio;
+            this.group = group;
+            this.year = year;
+        }
+        public static void FindByYear(Student st, string Year)
+        {
+            if (st.year == Year)
+            {
+                Console.WriteLine($"{st.fio} {st.group} {st.year}");
+            }
+        }
+        public static void FindByGroup(Student st, string Group)
+        {
+            if (st.group == Group)
+            {
+                Console.WriteLine(st.fio + " " + st.group + " " + st.year);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Student[] students = new Student[5];
+            students[0] = new Student("Иванов Иван Иванович", "ФИТ-231", "2002");
+            students[1] = new Student("Петров Петр Петрович", "ФИТ-231", "2004");
+            students[2] = new Student("Александров Александр Александрович", "ФИТ-231", "2010");
+            students[3] = new Student("Сергеев Сергей Сергеевич", "ФИТ-231", "2008");
+            students[4] = new Student("Васильев Василий Васильевич", "ФИТ-231", "2015");
+            Console.Write("Введите фильтр поиска 1 - Год рождения, 2 - Группа: ");
+            int fltr = Convert.ToInt32(Console.ReadLine());
+            switch (fltr)
+            {
+                case 1:
+                    Console.Write("Введите год рождения студента: ");
+                    string Year = Console.ReadLine();
+                    foreach (Student i in students)
+                    {
+                        Student.FindByYear(i, Year);
+                    }
+                    break;
+
+                case 2:
+                    Console.Write("Введите группу студента: ");
+                    string Group = Console.ReadLine();
+
+                    foreach (Student i in students)
+                    {
+                        Student.FindByGroup(i, Group);
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ LA/1Term/25.10.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Work_25._10
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите количество строк: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int maxLength = 0;
+            int[][] array = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Введите количество элементов в {i + 1} строке: ");
+                int length = Convert.ToInt32(Console.ReadLine());
+                maxLength += length;
+                Console.Write($"Введите элементы {i + 1} строки через пробел: ");
+                var inputElements = Console.ReadLine();
+                var outputElements = inputElements.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                array[i] = new int[length];
+                for (int j = 0; j < length; j++)
+                {
+                    array[i][j] = Convert.ToInt32(outputElements[j]);
+                }
+            }
+
+
+            //Пересечение
+            int intersectionSize = array[0].Length;
+            foreach (int i in array[0])
+            {
+                foreach (int[] j in array)
+                {
+                    if (!j.Contains(i))
+                    {
+                        intersectionSize--;
+                        break;
+                    }
+                }
+            }
+            int[] intersection = new int[intersectionSize];
+            int intersectionCount = 0;
+            foreach (int i in array[0])
+            {
+                bool res = true;
+                foreach (int[] j in array)
+                {
+                    if (!j.Contains(i))
+                    {
+                        res = false;
+                        break;
+                    }
+                }
+                if (res)
+                {
+                    intersection[intersectionCount] = i;
+                    intersectionCount++;
+                }
+            }
+            Console.WriteLine("Пересечение: [{0}]", string.Join(", ", (intersection)));
+
+            //Объединение
+            int[] union = new int[maxLength];
+            int count = 0;
+            foreach (int[] i in array)
+            {
+                foreach (int j in i)
+                {
+                    union[count] = j;
+                    count++;
+                }
+            }
+            int[] unionOutput = union.Distinct().ToArray();
+            Console.WriteLine("Объединение: [{0}]", string.Join(", ", (unionOutput)));
+
+            //Максимальные элементы
+            int maxElement = 0;
+            int[] maxElements = new int[n];
+            int countEl = 0;
+            foreach (int[] i in array)
+            {
+                foreach(int j in i)
+                {
+                    maxElement = Math.Max(maxElement, j);
+                }
+                maxElements[countEl] = maxElement;
+                countEl++;
+            }
+            Console.WriteLine("Максимальыне эелементы: [{0}]", string.Join(", ", (maxElements)));
+        }
+    }
+}
