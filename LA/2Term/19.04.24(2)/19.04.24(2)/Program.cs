78e94f0c8a0777f98e074cd88b7a1bc5d1b1c8aa
Sergey Motynga <motynga12345@mail.ru>
2024-05-08 19:24:43 +0600

    Задача

---
+++ LA/2Term/19.04.24(2)/19.04.24(2)/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _19._04._24_2_
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string inputFile = "input.txt";
+            string shortestASequence = null;
+
+            using (StreamReader reader = new StreamReader(inputFile))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    int currentLength = GetShortestASequenceLength(line);
+                    if (currentLength > 0 && (shortestASequence == null || currentLength < GetShortestASequenceLength(shortestASequence)))
+                    {
+                        shortestASequence = line;
+                    }
+                }
+            }
+            Console.WriteLine(shortestASequence ?? "No 'A' sequences found");
+        }
+        static int GetShortestASequenceLength(string s)
+        {
+            int shortestLength = int.MaxValue;
+            int currentLength = 0;
+            bool inSequence = false;
+            foreach (char c in s)
+            {
+                if (c == 'A')
+                {
+                    inSequence = true;
+                    currentLength++;
+                }
+                else if (inSequence)
+                {
+                    if (currentLength < shortestLength)
+                    {
+                        shortestLength = currentLength;
+                    }
+                    inSequence = false;
+                    currentLength = 0;
+                }
+            }
+            if (inSequence && currentLength < shortestLength)
+            {
+                shortestLength = currentLength;
+            }
+            return shortestLength == int.MaxValue ? 0 : shortestLength;
+        }
+    }
+}
