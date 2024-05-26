3cea8c2837e70a12c291327cdeebd96a7401eaeb
Sergey Motynga(notebook) <motynga12345@mail.ru>
2024-05-27 00:00:05 +0600

    Метод трапеций

---
+++ Pract/Work1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+class TrapezoidalRule
+{
+    public static double Solve(Func<double, double> f, double a, double b, double dx)
+    {
+        double epsilon = 10E-7;
+        if (!double.IsNormal(a)) { throw new ArgumentException("Начало интервала не является число"); }
+        if (!double.IsNormal(b)) { throw new ArgumentException("Конец интервала не является числом"); }
+        if (dx < epsilon) { throw new ArgumentException("dx меньше epsilon"); }
+
+        int intervals = Convert.ToInt32(Math.Ceiling((b - a) / dx));
+        double[] result = Enumerable.Range(0, intervals + 1).Select(i => a + dx * i).ToArray();
+
+        return result.Sum(x => Math.Abs(((f(x) + f(x + dx)) * dx) / 2));
+    }
+    static void Main(string[] args)
+    {
+        Func<double, double> f = (double x) => -x*x+9;
+        double result = TrapezoidalRule.Solve(f, -3, 3, 0.1);
+        Console.WriteLine(result);
+    }
+}
