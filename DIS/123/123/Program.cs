8a1893d7ba349146cb2a13b0d22fceb75f704dbf
Sergey Motynga <motynga12345@mail.ru>
2024-05-31 11:01:25 +0600

    RGR

---
+++ DIS/123/123/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace AlgorithmFordFulkerson
+{
+    internal class Program
+    {
+        static int FordFulkerson(List<int> capacity, Stack<int> used, int stock, int source, int[,] _adj, int[,] _un, int[,] _fil)
+        {
+            int interimSource = source;
+            int sum = 0;
+            Stack<int> usedN = new Stack<int>();
+            while (true)
+            {
+                bool next = false;
+                List<int> S = new List<int>() { 0 };
+                for (int i = 1; i < _un.GetLength(0); i++)
+                {
+                    if (used.Contains(i) || usedN.Contains(i)) { S.Add(0); continue; }
+                    S.Add(_un[interimSource, i]);
+                }
+                int max = S.Max();
+                while (true)
+                {
+                    if (max == 0)
+                    {
+                        if (used.Count == 0) return sum;
+                        S = new List<int>() { 0 };
+                        for (int i = 1; i < _fil.GetLength(0); i++)
+                        {
+                            if (used.Contains(i) || usedN.Contains(i)) { S.Add(0); continue; }
+                            S.Add(_fil[i, interimSource]);
+                        }
+                        max = S.Max();
+                        if (max == 0)
+                        {
+                            int a = used.Pop();
+                            usedN.Push(interimSource);
+                            interimSource = a;
+                            capacity.RemoveAt(capacity.Count - 1);
+                            next = true;
+                        }
+                        break;
+                    }
+                    else break;
+                }
+                if (next == true) continue;
+                capacity.Add(max);
+                used.Push(interimSource);
+                interimSource = S.IndexOf(max);
+                if (interimSource == stock)
+                {
+                    used.Push(interimSource);
+                    sum += capacity.Min();
+                    while (used.Count != 1)
+                    {
+                        int a = used.Pop();
+                        int b = used.Peek();
+                        _fil[b, a] += capacity.Min();
+                        _un[b, a] -= capacity.Min();
+                    }
+                    interimSource = source;
+                    capacity = new List<int>();
+                    used = new Stack<int>();
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            bool Return;
+            do
+            {
+                Return = true;
+                Console.Write($"1.Решение задачи\n2.Об авторе\n" +
+                    $"3.Помощь\n4.О программе\n5.Выход\nВыберете пункт: ");
+                int choose = Convert.ToInt32(Console.ReadLine());
+                if (choose != 1 && choose != 2 && choose != 3 && choose != 4 && choose != 5)
+                {
+                    Console.Write("Ошибка: такой пункт отсутствует" +
+                        "\nЧтобы попробовать заного 0, чтобы выйти из программы введите 1\nВаш ответ: ");
+                    choose = Convert.ToInt32(Console.ReadLine());
+                    if (choose == 1) { Return = false; }
+                    Console.Clear();
+                }
+                switch (choose)
+                {
+                    case 1:
+                        bool Return1;
+                        do
+                        {
+                            Console.Clear();
+                            Return1 = false;
+                            string input = @"C:\Users\motyn\OneDrive\Рабочий стол\project\123\123\bin\Debug\input.txt";
+                            string[] lines = File.ReadAllLines(input);
+                            int m = 0;
+                            int n = Convert.ToInt32(lines[0]);
+                            if (n < 1 || n > 100) throw new ArgumentException("Ошибка: количество промежуточных пунктов должно находится в диапазоне от 1 до 100 включительно.");
+                            int[,] matrixOfThroughputPerUnit = new int[n + 3, n + 3];
+                            int[,] matrixOfFilling = new int[n + 3, n + 3];
+                            int[,] adjacencyMatrix = new int[n + 3, n + 3];
+                            for (int i = 0; i < n + 2; i++)
+                            {
+                                matrixOfThroughputPerUnit[0, i + 1] = i + 1;
+                                matrixOfThroughputPerUnit[i + 1, 0] = i + 1;
+                                matrixOfFilling[0, i + 1] = i + 1;
+                                matrixOfFilling[i + 1, 0] = i + 1;
+                                adjacencyMatrix[0, i + 1] = i + 1;
+                                adjacencyMatrix[i + 1, 0] = i + 1;
+                            }
+                            int k = 0;
+                            foreach (string line in lines)
+                            {
+                                string[] lineInput = line.Split(' ');
+                                if (k == 0) { k++; continue; }
+                                if (lineInput[0].Equals("A"))
+                                {
+                                    matrixOfThroughputPerUnit[1, Convert.ToInt32(lineInput[1]) + 1] = Convert.ToInt32(lineInput[2]);
+                                    continue;
+                                }
+                                if (lineInput[1].Equals("B"))
+                                {
+                                    matrixOfThroughputPerUnit[Convert.ToInt32(lineInput[0]) + 1, n + 2] = Convert.ToInt32(lineInput[2]);
+                                    continue;
+                                }
+                                if (lineInput[0].Equals("A") && lineInput[1].Equals("B"))
+                                {
+                                    matrixOfThroughputPerUnit[1, n + 2] = Convert.ToInt32(lineInput[2]);
+                                    continue;
+                                }
+                                matrixOfThroughputPerUnit[Convert.ToInt32(lineInput[0]) + 1, Convert.ToInt32(lineInput[1]) + 1] = Convert.ToInt32(lineInput[2]);
+                                adjacencyMatrix[Convert.ToInt32(lineInput[0]) + 1, Convert.ToInt32(lineInput[1]) + 1] = 1;
+                            }
+                            int stock = n + 2;
+                            int source = 1;
+                            List<int> capacity = new List<int>();
+                            Stack<int> used = new Stack<int>();
+                            int sum = FordFulkerson(capacity, used, stock, source, adjacencyMatrix, matrixOfThroughputPerUnit, matrixOfFilling);
+                            Console.Write($"Суммарная величина объёма прокаченной нефти: {sum}" +
+                                $"\nЧтобы вернуться введите 0, чтобы выйти из программы введите 1\nВаш ответ: ");
+                            choose = Convert.ToInt32(Console.ReadLine());
+                            if (choose == 0) break;
+                        }
+                        while (Return1 == true);
+                        if (choose == 1) Return = false;
+                        Console.Clear();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Console.Write("Автор данной програмы: Мотынга Сергей Андреевич, студент ОмГТУ, ФИТ-231" +
+                            "\nЧтобы вернуться введите 0, чтобы выйти из программы введите 1\nВаш ответ: ");
+                        choose = Convert.ToInt32(Console.ReadLine());
+                        if (choose == 1) Return = false;
+                        Console.Clear();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Console.Write("На данный момент здесь пусто" +
+                            "\nЧтобы вернуться введите 0, чтобы выйти из программы введите 1\nВаш ответ: ");
+                        choose = Convert.ToInt32(Console.ReadLine());
+                        if (choose == 1) Return = false;
+                        Console.Clear();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Console.Write("Данная программа находит суммарный объём прокаченной нефти при помощи алгоритма Форда-Фалкерсона" +
+                            "\nЧтобы вернуться введите 0, чтобы выйти из программы введите 1\nВаш ответ: ");
+                        choose = Convert.ToInt32(Console.ReadLine());
+                        if (choose == 1) Return = false;
+                        Console.Clear();
+                        break;
+                    case 5:
+                        Return = false;
+                        break;
+                }
+            }
+            while (Return == true);
+        }
+    }
+}
+
+
---
+++ LA/RGR_ALGO/RGR_ALGO/Program.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGR_ALGO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Return;
+            do
+            {
+                Return = true;
+                Console.Write($"1.Задача про скобки\n2.Задача на польскую запись\n" +
+                    $"3.Об авторе\n4.О программе\n5.Помощь\n6.Выход\nВыберете пункт: ");
+                int choose = Convert.ToInt32(Console.ReadLine());
+                if (choose != 1 && choose != 2 && choose != 3 && choose != 4 && choose != 5 && choose != 6)
+                {
+                    Console.Write("Ошибка: такой пункт отсутствует" +
+                        "\nЧтобы попробовать заного 0, чтобы выйти из программы введите 1\nВаш ответ: ");
+                    choose = Convert.ToInt32(Console.ReadLine());
+                    if (choose == 1) { Return = false; }
+                    Console.Clear();
+                }
+                switch (choose)
+                {
+                    case 1:
+                        bool Return1;
+                        do
+                        {
+                            Console.Clear();
+                            Return1 = false;
+                            Console.Write("Введите выражение: ");
+                            string phrase = Console.ReadLine();
+                            Stack<char> stack = new Stack<char>();
+                            foreach (char c in phrase)
+                            {
+                                stack.Push(c);
+                            }
+                            bool result = true;
+                            for (int i = 0; i < (stack.Count - 1); i++)
+                            {
+                                if (stack.Count % 2 != 0) { result = false; break; }
+                                if (stack.ElementAt(i) == ')')
+                                {
+                                    if (stack.ElementAt(i + 1) != '(') { result = false; break; }
+                                }
+                                if (stack.ElementAt(i) == ']')
+                                {
+                                    if (stack.ElementAt(i + 1) != '[') { result = false; break; }
+                                }
+                                if (stack.ElementAt(i) == '}')
+                                {
+                                    if (stack.ElementAt(i + 1) != '{') { result = false; break; }
+                                }
+                            }
+                            if (stack.Count == 0)
+                            {
+                                Console.Write("Вы не ввели выражение" +
+                               "\nЧтобы вернуться введите 0, чтобы выйти из программы введите 1, " +
+                               "чтобы заного ввести выражение введите 2\nВаш ответ: ");
+                                choose = Convert.ToInt32(Console.ReadLine());
+                                if (choose == 2) Return1 = true;
+                                if (choose == 1) break;
+                                if (choose == 0) { Console.Clear(); break; }
+                            }
+                            if (Return1 == true) Console.WriteLine();
+                            if (result == false && Return1 == false)
+                            {
+                                Console.Write("Выражение некорректно" +
+                                    "\nЧтобы вернуться введите 0, чтобы выйти из программы введите 1, " +
+                                            "чтобы заного ввести выражение введите 2\nВаш ответ: ");
+                                choose = Convert.ToInt32(Console.ReadLine());
+                                if (choose == 2) Return1 = true;
+                                if (choose == 1) break;
+                            }
+                            else if (Return1 == false)
+                            {
+                                Console.Write("Выражение корректно" +
+                                    "\nЧтобы вернуться введите 0, чтобы выйти из программы введите 1, " +
+                                            "чтобы заного ввести выражение введите 2\nВаш ответ: ");
+                                choose = Convert.ToInt32(Console.ReadLine());
+                                if (choose == 2) Return1 = true;
+                                if(choose == 1) break;
+                            }
+                        }
+                        while (Return1 == true);
+                        if (choose == 1) Return = false;
+                        Console.Clear();
+                        break;
+                    case 2:
+                        bool Return2;
+                        do
+                        {
+                            Console.Clear();
+                            Return2 = false;
+                            Console.Write("Введите выражение: ");
+                            string phrase = Console.ReadLine();
+                            Stack<string> stack = new Stack<string>();
+                            double resultOfCalculated = 0, a, b;
+                            if (phrase == "")
+                            {
+                                Console.Write("Вы не ввели выражение" +
+                                "\nЧтобы вернуться введите 0, чтобы выйти из программы введите 1, " +
+                                "чтобы заного ввести выражение введите 2\nВаш ответ: ");
+                                choose = Convert.ToInt32(Console.ReadLine());
+                                if (choose == 2) Return2 = true;
+                                if (choose == 1) break;
+                                if (choose == 0) { Console.Clear(); break; }
+                            }
+                            if (phrase.Length < 3)
+                            {
+                                Console.Write("Ошибка: в выражении отсутствуют элементы для вычисления" +
+                                "\nЧтобы вернуться введите 0, чтобы выйти из программы введите 1, " +
+                                "чтобы заного ввести выражение введите 2\nВаш ответ: ");
+                                choose = Convert.ToInt32(Console.ReadLine());
+                                if (choose == 2) { Return2 = true; }
+                                if (choose == 1) break;
+                                if (choose == 0) { Console.Clear(); break; }
+                            }
+                            foreach (char c in phrase)
+                            {
+                                if (c == '+' || c == '-' || c == '*' || c == '/')
+                                {
+                                    if (stack.Count < 2)
+                                    {
+                                        Console.Write("Ошибка: в выражении отсутствуют элементы для вычисления" +
+                                             "\nЧтобы вернуться введите 0, чтобы выйти из программы введите 1, " +
+                                             "чтобы заного ввести выражение введите 2\nВаш ответ: ");
+                                        choose = Convert.ToInt32(Console.ReadLine());
+                                        if (choose == 2) { Return2 = true; break; }
+                                    }
+                                    a = Convert.ToDouble(stack.Pop());
+                                    b = Convert.ToDouble(stack.Pop());
+                                    if (c == '/' && (a == 0 || b == 0))
+                                    {
+                                        Console.Write("Ошибка: деление на 0 невозможно" +
+                                            "\nЧтобы вернуться введите 0, чтобы выйти из программы введите 1, " +
+                                            "чтобы заного ввести выражение введите 2\nВаш ответ: ");
+                                        choose = Convert.ToInt32(Console.ReadLine());
+                                        if (choose == 2) { Return2 = true; break; }
+                                    }
+                                    if (c == '+') resultOfCalculated = b + a;
+                                    if (c == '-') resultOfCalculated = b - a;
+                                    if (c == '*') resultOfCalculated = b * a;
+                                    if (c == '/') resultOfCalculated = b / a;
+                                    stack.Push(Convert.ToString(resultOfCalculated));
+                                }
+                                else
+                                {
+                                    if (c != ' ') stack.Push(Convert.ToString(c));
+                                }
+                            }
+                            if (Return2 == true) Console.Clear();
+                            else
+                            {
+                                Console.Write($"Ответ: {resultOfCalculated}" +
+                                $"\nЧтобы вернуться введите 0, чтобы выйти из программы введите 1, " +
+                                            "чтобы заного ввести выражение введите 2\nВаш ответ: ");
+                                choose = Convert.ToInt32(Console.ReadLine());
+                            }
+                        }
+                        while (Return2 == true);
+                        if (choose == 1) Return = false;
+                        Console.Clear();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Console.Write("Автор данной програмы: Мотынга Сергей Андреевич, студент ОмГТУ, ФИТ-231" +
+                            "\nЧтобы вернуться введите 0, чтобы выйти из программы введите 1\nВаш ответ: ");
+                        choose = Convert.ToInt32(Console.ReadLine());
+                        if (choose == 1) Return = false;
+                        Console.Clear();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Console.Write("Данная программа выполняет 2 задачи:\n1-я проверка строки на првильность" +
+                            " расстановки скобок, к примеру () - правильно, а )( - неправильно." +
+                            "\n2-я решает математическое выражение, введённое при помощи польской записи." +
+                            "\nЧтобы вернуться введите 0, чтобы выйти из программы введите 1\nВаш ответ: ");
+                        choose = Convert.ToInt32(Console.ReadLine());
+                        if (choose == 1) Return = false;
+                        Console.Clear();
+                        break;
+                    case 5:
+                        Console.Clear();
+                        Console.Write("На данный момент здесь пусто" +
+                            "\nЧтобы вернуться введите 0, чтобы выйти из программы введите 1\nВаш ответ: ");
+                        choose = Convert.ToInt32(Console.ReadLine());
+                        if (choose == 1) Return = false;
+                        Console.Clear();
+                        break;
+                    case 6:
+                        Return = false;
+                        break;
+                }
+            }
+            while (Return == true);
+        }
+    }
+}
